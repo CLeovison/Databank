b4777db6a03f52480f5990c0c0cf3e79992e516e
Leovison <cleovison@gmail.com>
2024-12-18 18:53:15 +0800

    feat: Implementing Repository Pattern

--- Repositories/IUserRespositories.cs
+++ Repositories/IUserRespositories.cs
@@ -7,6 +7,10 @@ public interface IUserRepositories
 {
     Task<bool> CreateUserAsync(UserDto user);
     Task<UserDto?> GetUserAsync(Guid id);
-
+    Task<IEnumerable<UserDto>> GetAllUserAsync();
+    Task<bool> UpdateUserAsync(UserDto user);
+    Task<bool> DeleteUserAsync(Guid id);
+    Task<bool> ResetUserPasswordAsync(Guid id);
+    Task<bool> ForgotUserPassswordAsync(Guid id);
 
 }
